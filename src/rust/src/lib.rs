8c999c65f67887477ad5a5e351f03dc7f074abea
BrftM <moritz.broft@student.uibk.ac.at>
2025-06-26 16:26:47 +0200

    Remove comments

--- src/rust/src/lib.rs
+++ src/rust/src/lib.rs
@@ -6,28 +6,22 @@ use std::process::{Command};
 /// @export
 #[extendr]
 fn guidecounter_count(
-    input: Vec<String>,               // List of input files (as strings, R will pass these as character vectors)
-    library: String,                   // Path to the library (as a string, R will pass this as a character vector)
-    offset_min_fraction: f64,          // Offset minimum fraction
-    output: String                     // Output directory
+    input: Vec<String>,             
+    library: String,                  
+    offset_min_fraction: f64,          
+    output: String                     
 ) -> String {
-    // Convert ininputputs and library to PathBuf
-
-    // Build the `fqtk demux` command using the provided parameters
     let mut command = Command::new("guide-counter");
     command.arg("count");
 
-    // Add inputs (path to input files)
     for input_path in input {
                 command.arg("--input").arg(input_path);
     }
 
-    // Add other parameters
     command.arg("--offset-min-fraction").arg(offset_min_fraction.to_string())
             .arg("--library").arg(library)
             .arg("--output").arg(output);
 
-    // Execute the command
     match command.output() {
         Ok(output) => {
             if output.status.success() {
@@ -41,9 +35,6 @@ fn guidecounter_count(
     }
 }
 
-// Macro to generate exports.
-// This ensures exported functions are registered with R.
-// See corresponding C code in `entrypoint.c`.
 extendr_module! {
     mod guideCounterWrapper;
     fn guidecounter_count;
