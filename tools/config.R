b374c2f7411655ee88823b35c53fc59349515d57
BrftM <moritz.broft@student.uibk.ac.at>
2025-07-16 11:46:35 +0200

    Add-suggests-and-conf-build

--- tools/config.R
+++ tools/config.R
@@ -1,22 +1,22 @@
 # Note: Any variables prefixed with `.` are used for text
 # replacement in the Makevars.in and Makevars.win.in
 
-# check the packages MSRV first
+# Check the packages MSRV first
 source("tools/msrv.R")
 
-# check DEBUG and NOT_CRAN environment variables
+# Check DEBUG and NOT_CRAN environment variables
 env_debug <- Sys.getenv("DEBUG")
 env_not_cran <- Sys.getenv("NOT_CRAN")
 
-# check if the vendored zip file exists
+# Check if the vendored zip file exists
 vendor_exists <- file.exists("src/rcounter/vendor.tar.xz")
 
 is_not_cran <- env_not_cran != ""
 is_debug <- env_debug != ""
 
 if (is_debug) {
-  # if we have DEBUG then we set not cran to true
-  # CRAN is always release build
+  # If we have DEBUG then we set not cran to TRUE
+  # CRAN is always a release build
   is_not_cran <- TRUE
   message("Creating DEBUG build.")
 }
@@ -25,71 +25,71 @@ if (!is_not_cran) {
   message("Building for CRAN.")
 }
 
-# we set cran flags only if NOT_CRAN is empty and if
+# We set CRAN flags only if NOT_CRAN is empty and if
 # the vendored crates are present.
 .cran_flags <- ifelse(
-  !is_not_cran && vendor_exists,
-  "-j 2 --offline",
+  vendor_exists,
+  "-j 2 --offline",  # Enable offline builds if vendored crates exist
   ""
 )
 
-# when DEBUG env var is present we use `--debug` build
+# When DEBUG environment variable is present we use `--debug` build
 .profile <- ifelse(is_debug, "", "--release")
 .clean_targets <- ifelse(is_debug, "", "$(TARGET_DIR)")
 
 # We specify this target when building for webR
 webr_target <- "wasm32-unknown-emscripten"
 
-# here we check if the platform we are building for is webr
+# Here we check if the platform we are building for is webR
 is_wasm <- identical(R.version$platform, webr_target)
 
-# print to terminal to inform we are building for webr
+# Print to terminal to inform we are building for webR
 if (is_wasm) {
   message("Building for WebR")
 }
 
-# we check if we are making a debug build or not
-# if so, the LIBDIR environment variable becomes:
+# We check if we are making a debug build or not
+# If so, the LIBDIR environment variable becomes:
 # LIBDIR = $(TARGET_DIR)/{wasm32-unknown-emscripten}/debug
-# this will be used to fill out the LIBDIR env var for Makevars.in
+# This will be used to fill out the LIBDIR env var for Makevars.in
 target_libpath <- if (is_wasm) "wasm32-unknown-emscripten" else NULL
 cfg <- if (is_debug) "debug" else "release"
 
-# used to replace @LIBDIR@
+# Used to replace @LIBDIR@
 .libdir <- paste(c(target_libpath, cfg), collapse = "/")
 
-# use this to replace @TARGET@
-# we specify the target _only_ on webR
-# there may be use cases later where this can be adapted or expanded
+# Use this to replace @TARGET@
+# We specify the target _only_ on webR
+# There may be use cases later where this can be adapted or expanded
 .target <- ifelse(is_wasm, paste0("--target=", webr_target), "")
 
-# read in the Makevars.in file checking
+# Read in the Makevars.in file checking
 is_windows <- .Platform[["OS.type"]] == "windows"
 
-# if windows we replace in the Makevars.win.in
+# If Windows, we replace in the Makevars.win.in
 mv_fp <- ifelse(
   is_windows,
   "src/Makevars.win.in",
   "src/Makevars.in"
 )
 
-# set the output file
+# Set the output file
 mv_ofp <- ifelse(
   is_windows,
   "src/Makevars.win",
   "src/Makevars"
 )
 
-# delete the existing Makevars{.win}
+# Delete the existing Makevars{.win}
 if (file.exists(mv_ofp)) {
   message("Cleaning previous `", mv_ofp, "`.")
   invisible(file.remove(mv_ofp))
 }
 
-# read as a single string
+# Read as a single string
 mv_txt <- readLines(mv_fp)
 
-# replace placeholder values
+# Replace placeholder values
 new_txt <- gsub("@CRAN_FLAGS@", .cran_flags, mv_txt) |>
   gsub("@PROFILE@", .profile, x = _) |>
   gsub("@CLEAN_TARGET@", .clean_targets, x = _) |>
